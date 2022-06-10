347d59d828e43ab9f4a905c4aaea4afbfbb6aff8
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-10 16:15:35 -0700

    added my test, made the data file and fixed some small spelling errors

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,32 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { consoles } = require('../data/console');
+
+describe('console routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/consoles should show the list of consoles', async () => {
+    const res = await request(app).get('/consoles');
+    const expected = consoles.map((console) => {
+      return { id: console.id, name: console.name };
+    });
+    expect(res.body).toEqual(expected);
   });
+
+  it('/consoles/:id should show the details of a consoles', async () => {
+    const res = await request(app).get('/consoles/1');
+    const atari = {
+      id: '1',
+      name: 'Atari',
+      description: 'Classically viewed as the original game console.',
+      year: 1977,
+    };
+    expect(res.body).toEqual(atari);
+  });
+
   afterAll(() => {
     pool.end();
   });
